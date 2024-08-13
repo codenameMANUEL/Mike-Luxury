00c3df81cfa4437d83123f5b4ed31eb130cb4dde
codenameManuel <manuelfranky0@gmail.com>
2024-08-13 20:39:18 +0100

    fixing the nav bar for all views

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+import {
+  UserRound,
+  ShoppingBag,
+  AlignLeft,
+  X,
+  Facebook,
+  Instagram,
+} from "lucide-react";
+import LOGO from "../assets/logo.png";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faWhatsapp } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
+
+export default function NavBar() {
+  const [isTabOpen, setIsTabOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const toggleTab = () => setIsTabOpen(!isTabOpen);
+  const links = ["Home", "Shop", "Marketplace", "Company"];
+
+  const tester = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <nav className="relative bg-white shadow-sm w-full">
+      <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8 md:hidden">
+        <div className="flex flex-1 justify-start">
+          <AlignLeft
+            className="h-6 w-6 text-stone-800 cursor-pointer"
+            onClick={toggleMenu}
+          />
+        </div>
+        <div className="flex flex-1 justify-center">
+          <img alt="Logo" src={LOGO} className="h-10 w-auto" />
+        </div>
+        <div className="flex flex-1 justify-end space-x-4">
+          <ShoppingBag className="h-6 w-6 text-stone-800" onClick={tester} />
+          <span className="border-solid bg-red-500 px-[.5rem] rounded-[2rem] absolute top-3 right-3 text-[.7rem] w-[1rem] flex items-center justify-center text-white">
+            7
+          </span>
+        </div>
+      </div>
+
+      {isMenuOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 z-40"
+          onClick={toggleMenu}
+        />
+      )}
+
+      <div
+        className={`fixed inset-y-0 left-0 bg-white shadow-lg transform z-50 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out w-6/12`}
+      >
+        <div className="flex items-center justify-between p-4">
+          <span className="text-lg font-semibold"></span>
+          <button onClick={toggleMenu} className="text-stone-800">
+            <X className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="p-4">
+          <ul className="space-y-7">
+            {links.map((link, index) => (
+              <li key={index} className="text-gray-800 hover:text-gray-600">
+                <a href={`#${link.toLowerCase()}`}>{link}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="pt-12">
+            <button className="rounded bg-gray-300 flex items-center justify-center w-32 h-10 gap-2 hover:bg-black hover:text-white">
+              <UserRound className="" /> Login
+            </button>
+          </div>
+          <div className="">
+            <div className="flex space-x-4 pl-4 absolute inset-x-0 bottom-5 border-t pt-4 border-gray-300">
+              <span className="inline-block p-2 bg-sky-500 rounded-full hover:bg-sky-700 cursor-pointer">
+                <Facebook strokeWidth={1} className="text-white" />
+              </span>
+              <span className="inline-block p-2 bg-gradient-to-r from-instagram-start to-instagram-end rounded-full hover:opacity-80 cursor-pointer">
+                <Instagram
+                  strokeWidth={1}
+                  className="text-gray-800 cursor-pointer"
+                />
+              </span>
+              <span>
+                <Facebook
+                  strokeWidth={1}
+                  className="text-gray-800 cursor-pointer"
+                />
+                {/* <FontAwesomeIcon icon={faWhatsapp} /> */}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden md:flex lg:hidden items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-1 justify-start">
+          <AlignLeft className="h-6 w-6 text-stone-800" onClick={toggleTab} />
+        </div>
+        <div className="flex flex-1 justify-center">
+          <img alt="Logo" src={LOGO} className="h-12 w-auto" />
+        </div>
+        <div className="flex flex-1 justify-end space-x-4">
+          <UserRound className="h-6 w-6 text-stone-800" />
+          <ShoppingBag className="h-6 w-6 text-stone-800" />
+          <span className="border-solid bg-red-500 px-[.5rem] rounded-[2rem] absolute top-3 right-3 text-[.7rem] w-[1rem] flex items-center justify-center text-white">
+            7
+          </span>
+        </div>
+      </div>
+
+      <div
+        className={`fixed inset-y-0 left-0 bg-white shadow-lg transform z-50 ${
+          isTabOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out w-9/12`}
+      >
+        <div className="flex items-center justify-between p-4">
+          <span className="text-lg font-semibold"></span>
+          <button onClick={toggleTab} className="text-stone-800">
+            <X className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="p-4">
+          <ul className="space-y-4">
+            <li>
+              <a href="#" className="text-gray-800">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-800">
+                Shop
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-800">
+                Marketplace
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-800">
+                Company
+              </a>
+            </li>
+          </ul>
+          <div className="pt-12">
+            <button className="rounded bg-gray-300 flex items-center justify-center w-32 h-10 gap-2 hover:bg-black hover:text-white">
+              <UserRound className="" /> Login
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-1 justify-start">
+          <img alt="Logo" src={LOGO} className="h-12 w-auto" />
+        </div>
+        <div className="flex flex-1 justify-center space-x-10">
+          <a href="#" className="text-stone-800">
+            Home
+          </a>
+          <a href="#" className="text-stone-800">
+            Shop
+          </a>
+          <a href="#" className="text-stone-800">
+            Marketplace
+          </a>
+          <a href="#" className="text-stone-800">
+            Company
+          </a>
+        </div>
+        <div className="flex flex-1 justify-end space-x-6">
+          <UserRound className="h-6 w-6 text-stone-800" />
+          <ShoppingBag className="h-6 w-6 text-stone-800" />
+          <span className="border-solid bg-red-500 px-[.5rem] rounded-[2rem] absolute top-3 right-3 text-[.7rem] w-[1rem] flex items-center justify-center text-white">
+            7
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        albert: ['"Albert Sans"', 'sans-serif'],
+      },
+      colors: {
+        'instagram-start': '#f58529',
+        'instagram-end': '#dd2a7b',
+      },
+    },
+  },
+  plugins: [],
+};
